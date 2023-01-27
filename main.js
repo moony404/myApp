b9beabaab9d7ccecbdd9e0b8a74d778ead0610e0
moony404 <theo.marliac63@gmail.com>
2023-01-27 19:39:10 +0100

    added the createMainWindow function for creating the main window

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const { app, BrowerWindow } =
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        // placeholder
+        title: 'myApp',
+        width: 500,
+        height: 600
+    });
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+app.whenReady().then(() => {
+    createMainWindow();
+});
