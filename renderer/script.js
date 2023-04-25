b50e38adbdd5bc9df7181707e9b1b4c8aec2f59c
moony404 <theo.marliac63@gmail.com>
2023-04-25 19:40:31 +0200

    rework of myApp part 1

---
+++ renderer/script.js
@@ -0,0 +1,11 @@
+const downloadBtn = document.getElementById('downloadButton');
+downloadBtn.addEventListener('click', () => {
+  const fileUrl = 'https://example.com/file.pdf'; // Replace with the URL of the file you want to download
+  const fileName = 'myfile.pdf'; // Replace with the desired name of the downloaded file
+  const link = document.createElement('a');
+  link.href = fileUrl;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
