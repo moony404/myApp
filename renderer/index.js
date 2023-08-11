94edeed0e2ef20e26b295d7d416260c9a910e545
moony404 <theo.marliac63@gmail.com>
2023-08-11 19:11:07 +0200

    upt

--- renderer/index.js
+++ renderer/index.js
@@ -4,4 +4,7 @@ const resultDiv = document.getElementById('result');
 versionSelect.addEventListener('change', () => {
   const selectedVersion = versionSelect.value;
   resultDiv.textContent = `Vous avez choisi la ${selectedVersion}.`;
+  if (selectedVersion == 'ZapoomC_v1.2') {
+
+  }
 });
