639bac77895ffa14db0e212ac3ef1c1b892bc2f8
moony404 <theo.marliac63@gmail.com>
2023-08-11 19:18:20 +0200

    added the menu for version selection

--- renderer/index.js
+++ renderer/index.js
@@ -3,14 +3,11 @@ const versionSelect = document.getElementById('version-select');
 
 downloadButton.addEventListener('click', () => {
     const selectedVersion = versionSelect.value;
-    if (selectedVersion === 'version1') {
+    if (selectedVersion === 'ZapoomC_v1.2') {
         // Code pour télécharger la version 1
-        window.location.href = 'lien_vers_version_1';
+        window.location.href = 'ZapoomC copy/ZapoomC_v1.2';
     } else if (selectedVersion === 'version2') {
         // Code pour télécharger la version 2
-        window.location.href = 'lien_vers_version_2';
-    } else if (selectedVersion === 'version3') {
-        // Code pour télécharger la version 3
-        window.location.href = 'lien_vers_version_3';
+        window.location.href = 'ZapoomC copy/ZapoomC_v1.2';
     }
 });
