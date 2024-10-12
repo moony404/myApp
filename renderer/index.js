d22923d05c1fcd4e3dff7db1ed84c086e58f8c1f
moony404 <theo.marliac63@gmail.com>
2024-10-12 15:34:50 +0200

    renamed dwnSrc to downSrc + changed paths

--- renderer/index.js
+++ renderer/index.js
@@ -5,9 +5,9 @@ downloadButton.addEventListener('click', () => {
     const selectedVersion = versionSelect.value;
     if (selectedVersion === 'ZapoomC_v1.2') {
         // Code pour télécharger la version 1
-        window.location.href = 'ZapoomC copy/ZapoomC_v1.2';
+        window.location.href = 'downSrc/ZapoomC_v1.2';
     } else if (selectedVersion === 'ZapoomC_v1.3') {
         // Code pour télécharger la version 2
-        window.location.href = 'dwnSrc/test.txt';
+        window.location.href = 'downSrc/test.txt';
     }
 });
